b4ab59e99fba6e0f80091e3ea52f123165789d13
Rustam Ibragimov <0exp@users.noreply.github.com>
2018-12-23 15:33:47 +0400

    [ci] spec helper with initial configuration

---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+])
+
+SimpleCov.start { add_filter 'spec' }
+
+require 'bundler/setup'
+require 'rabbit_matcher'
+require 'pry'
+
+RSpec.configure do |config|
+  config.order = :random
+  Kernel.srand config.seed
+  config.expect_with(:rspec) { |c| c.syntax = :expect }
+end
